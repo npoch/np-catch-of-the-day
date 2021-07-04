27bd0c645e4da98305fc71c66920cb58bb2a7d6e
npoch <nathanpoch@gmail.com>
2021-07-04 14:42:22 -0400

    full commit

---
+++ src/components/Fish.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import { formatPrice } from '../helpers';
+import PropTypes from 'prop-types';
+
+
+class Fish extends Component {
+    static propTypes = {
+        details: PropTypes.shape({
+            image: PropTypes.string,
+            name: PropTypes.string,
+            desc: PropTypes.string,
+            status: PropTypes.string,
+            price: PropTypes.number
+            }),
+        addToOrder: PropTypes.func
+    }
+    handleClick = () => {
+        this.props.addToOrder(this.props.index);
+    }
+    render() { 
+        const { image, name, price, desc, status } = this.props.details;
+        const isAvailable = status === "available";
+        return (
+            <li className="menu-fish" >
+                <img src={image} alt={name} />
+                <h3 className="fish-name">{name}
+                <span className="price">{formatPrice(price)}</span>
+                </h3>
+                <p>{desc}</p>
+                <button 
+                disabled={!isAvailable} 
+                onClick={this.handleClick}>
+                {isAvailable ? 'Add To Order' : 'Sold Out !'}
+                </button>
+            </li>
+        );
+    }
+}
+ 
+export default Fish;
---
+++ src/components/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+    <div>
+        <p></p>
+        <h2>AHOY, The page ye be lookin' fer is not on the map... YARR!</h2>
+    </div>
+    );
+}
+ 
+export default NotFound;
